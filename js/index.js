5611b7ffc2d6ae4ac90bfc6ba67ce3de25687aae
Jeffrey <jeffkim1118@outlook.com>
2021-11-29 21:36:13 -0500

    display for the weather info added

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
-const apiKey = "4dca84778eff4ae8e66e7cc29b12f1b4"
+window.onload = function(){
+    const apiKey = "4dca84778eff4ae8e66e7cc29b12f1b4"
+    let button = document.querySelector('#submit')
+    let cityName = document.querySelector('.name')
+    let desc = document.querySelector('.desc')
+    let temp = document.querySelector('.temp')
 
-let cityName = document.querySelector('.name')
-let desc = document.querySelector('.desc')
-let temp = document.querySelector('.temp')
 
-document.addEventListener('click', function(){
-    let inputValue = document.getElementById('inputValue').value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}`)
-    .then(res => res.json())
-    .then(data => {
-        let nameValue = data['name'];
-        let tempValue = data['main']['temp'];
-        let descValue = data['weather'][1]['description'];
+    button.addEventListener('click', ()=>{
+        let inputValue = document.getElementById('inputValue').value;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=imperial&appid=${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+            let nameValue = data['name'];
+            let tempValue = data['main']['temp'];
+            let descValue = data['weather'][0]['description'];
+    
+            cityName.innerHTML = nameValue;
+            temp.innerHTML = tempValue+'&#x2109;';
+            desc.innerHTML = descValue.toUpperCase();
+        })
 
-        cityName.innerHTML = nameValue;
-        temp.innerHTML = tempValue;
-        desc.innerHTML = descValue;
+        
     })
-})
+}
+
+
+
 
 
 
